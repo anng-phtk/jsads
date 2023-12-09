71c0439956d0359bb8975607453a9e778d1f22c2
anng-phtk <anngphtk@gmail.com>
2023-12-09 14:56:29 -0600

    LC:345 - reverse vowels in a word solution, 2nd version

--- strings/345-reverseVowels.js
+++ strings/345-reverseVowels.js
@@ -10,26 +10,24 @@ const reverseVowels = s => {
     let [leftPtr, rightPtr] = [0, temp.length-1];
     
     while (leftPtr < rightPtr) {
-        // advance the pointers if value is not a vowel
-        if (!vowels.has(temp[leftPtr])) {
-            leftPtr++
-        }
-
-        if (!vowels.has(temp[rightPtr])) {
-            rightPtr--;
-        }
-
-        // if both are vowels then swap and resume
-        if (vowels.has(temp[leftPtr]) && vowels.has(temp[rightPtr])) {
-            //when value is a vowel do the swap
-            const tempPlaceholder = temp[rightPtr];
-            temp[rightPtr] = temp[leftPtr];
-            temp[leftPtr] = tempPlaceholder;
-            // now advance pointers
-
+        //advance pointers till they hit a vowel
+        while (!vowels.has(temp[leftPtr]) && leftPtr < rightPtr) {
             leftPtr++;
+        }
+
+        while (!vowels.has(temp[rightPtr]) && leftPtr < rightPtr) {
             rightPtr--;
         }
+
+        // if vowels are found at both pointers do the swap
+        //when value is a vowel do the swap
+        const tempPlaceholder = temp[rightPtr];
+        temp[rightPtr] = temp[leftPtr];
+        temp[leftPtr] = tempPlaceholder;
+        // now advance pointers
+
+        leftPtr++;
+        rightPtr--;
 
     }
 
@@ -37,4 +35,4 @@ const reverseVowels = s => {
     
 };
 
-console.reverseVowels("Hello")
+console.log(reverseVowels("Hello"));
