cb048900d68b2bc1363b045c6871e3345da6b208
Anng Phtk <150362499+anng-phtk@users.noreply.github.com>
2025-12-17 22:41:34 -0600

    solved advanced sliding window problems. 159 was a tough one to get right. but ! did it

---
+++ jsts/Mechanics/Two_Pointers/Sliding_Window/159.Longest-Substring-At-Most-Two-Distinct-Char copy.ts
@@ -0,0 +1,114 @@
+const longestSubstr = (s:string): number => {
+    // if only 2, 1 or 0 elements in the array (array len = 0 to 1), then thats the max length
+    if (s.length <= 1) return s.length; 
+    
+    let result: number = 0;
+    let charFreq:Array<number> = new Array<number>(26).fill(0);
+    // helper to convert a letter to charcode
+    const getIdx = (letter:string):number => {
+        return (letter.charCodeAt(0) - 'a'.charCodeAt(0));
+    }
+    let [l,r]:[number, number] = [0,0];   
+    while (r < s.length) {
+        let rCharIdx: number = getIdx(s[r]);
+        // we saw it, account for it
+        charFreq[rCharIdx]++;
+
+        let numDistinctSeen: number = 0;
+        // we need exactly 2 distinct chars in a string
+        for (let i: number = 0; i < charFreq.length; i++) {
+            if (charFreq[i] > 0) {
+                numDistinctSeen++;
+            }
+        }
+
+        // shrink the window now
+        while (numDistinctSeen > 2) {
+            let lCharIdx: number = getIdx(s[l]);
+            charFreq[lCharIdx]--;
+            l++;
+
+            let temp:number = 0;
+            for (let i: number = 0; i < charFreq.length; i++) {
+                if (charFreq[i] > 0) {
+                    temp++;
+                }
+            }
+            numDistinctSeen = temp;
+            // we skip remaining execution
+            // we could have run a while loop instead of the if, but this seems clearer that only l window is reduced
+        }
+
+        result = Math.max(result, r-l+1);
+        r++;
+    }
+
+    return result;
+}
+
+
+//let str: string = "abababab";
+let str: string = "eceba";
+console.log('1. result = ', longestSubstr(str));
+
+str = "ccaabbb";
+console.log('2. result = ', longestSubstr(str));
+
+str = "aabcabbbbb";
+console.log('3. result = ', longestSubstr(str));
+
+
+str = "abaccc";
+console.log('4. result = ', longestSubstr(str));
+
+
+let s = "abababab" // → 8
+console.log('5. result = ', longestSubstr(s));
+s = "ababa" //→ 5
+console.log('6. result = ', longestSubstr(s));
+
+
+s = "aaabbbccc"; // → 6 (“aaabbb” or “bbbccc”)
+console.log('7. result = ', longestSubstr(s));
+s = "abcbbbbcccbdddadacb"; // → 10 (this is a known stress case)
+console.log('8. result = ', longestSubstr(s));
+
+s = "bbbbbbbb" //→ 8
+console.log('9. result = ', longestSubstr(s));
+
+s = "abcdef" //→ 2
+console.log('10. result = ', longestSubstr(s));
+
+
+console.log('sanity. result = ', longestSubstr(""));
+
+console.log('sanity. result = ', longestSubstr("a"));
+
+console.log('sanity. result = ', longestSubstr("aa"));
+
+console.log('sanity. result = ', longestSubstr("ab"));
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * let charFreq:Array<number> = new Array<number>(26).fill(0); // fixed arr size = o(1) space
+    let numDistinctSeen:number = 0;
+abc
+    // helper to convert a letter to charcode
+    const getCharIdx = (letter:string):number => {
+        return (letter.charCodeAt(0) - 'a'.charCodeAt(0));
+    }
+    // lets find out how many distinct chars are there
+    for (let i: number = 0; i < s.length; i++) {
+        let charIdx: number = getCharIdx(s[i]);
+        charFreq[charIdx]++;
+    }
+
+    // now we know
+    charFreq.forEach((val:number, idx:number)=> {
+        if (val > 0) numDistinctSeen++;
+    });
+ */
---
+++ jsts/Mechanics/Two_Pointers/Sliding_Window/159.Longest-Substring-With-1Repeat.ts
@@ -0,0 +1,70 @@
+const longestSubstr1Rpt = (s:string): number => {
+    // no need to perform the rest of the work if the string is empty or only 1 char long
+    if (s.length <= 1) return s.length;
+
+    let result: number = 0;
+
+
+    let charFreqArr: number[] = new Array<number>(26).fill(0); // fixed size, so O(1) space
+    let lastSeenTwice:number = 0; //[] = new Array<number>(2).fill(-1); // this is 2 long, so O(1) space 
+
+    let [left, right]:[number, number] = [0,0];
+
+
+    const getIdx = (alphabet:string): number => {
+        return alphabet.charCodeAt(0) - 'a'.charCodeAt(0);
+    }
+
+    while (left < s.length && right < s.length) {
+
+        let rCharIdx:number = getIdx(s[right]);
+        
+
+        // we saw it, so lets account for it
+        charFreqArr[rCharIdx]++;
+
+        for (let i = 0; i < 26; i++) {
+            if (charFreqArr[i] === 2) {
+                lastSeenTwice++;
+
+                // no need to loop if we see 2 pairs
+                if (lastSeenTwice === 2) break;
+            }
+        }
+
+        if (lastSeenTwice === 2) {
+            // we should capture the string lenght here
+            result = Math.max(result, right-left);
+
+            // now prepare to move the left pointer
+            while (lastSeenTwice === 2) {
+                let lcharIdx: number = getIdx(s[left]);
+
+                // we saw it, so lets account for it
+                charFreqArr[lcharIdx]--;
+                left++;
+
+                let temp: number = 0;
+                for (let i = 0; i < 26; i++) {
+                    if (charFreqArr[i] === 2) {
+                        temp++;
+                    }
+                }
+
+                lastSeenTwice = temp;
+
+            }
+            
+        }
+
+        right++;
+    }
+
+    result = Math.max(result, right-left);
+    
+    return result;
+}
+
+let str: string = "abba";
+
+console.log('result = ', longestSubstr1Rpt(str));
